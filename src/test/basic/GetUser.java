2d910e953481f7807f02a1fe29e5acfb1be47dd5
Vini5577 <ugopessoa@gmail.com>
2024-02-19 11:23:46 -0300

    add getUser e updateUser

---
+++ src/test/basic/GetUser.java
@@ -0,0 +1,21 @@
+package test.basic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.basic.User;
+
+public class GetUser {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        User user = em.find(User.class, 7L);
+        System.out.println("\n Nome: " + user.getName() + "\n");
+
+        em.close();
+        emf.close();
+    }
+}
---
+++ src/test/basic/UpdateUser1.java
@@ -0,0 +1,28 @@
+package test.basic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.basic.User;
+
+public class UpdateUser1 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        User user = em.find(User.class, 7L);
+        user.setName("Leonardo");
+        user.setEmail("leonardo@lanche.com.br");
+
+        em.merge(user);
+
+        em.getTransaction().commit();
+
+        emf.close();
+        em.close();
+    }
+}
