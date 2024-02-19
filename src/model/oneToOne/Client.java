4c047dcbe709d0e71eefdde8c8c565c1c7375895
Vini5577 <ugopessoa@gmail.com>
2024-02-19 11:20:28 -0300

    correction in files

--- src/model/oneToOne/Client.java
+++ src/model/oneToOne/Client.java
@@ -1,7 +1,6 @@
 package model.oneToOne;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
